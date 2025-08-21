3018b9d2ad6305f7dedbb382b91af54ebcfab585
Chee <tehcheeyeo@gmail.com>
2025-08-21 16:21:35 +0800

    fix: remove config for monorepo deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,17 +11,6 @@ app.use(express.json());
 app.use("/api/fruits", fruitsRouter);
 app.use("/api/orders", ordersRouter);
 
-if (process.env.NODE_ENV === "production") {
-  import("path").then((path) => {
-    import("express").then((express) => {
-      const __dirname = path.resolve();
-      app.use(express.static(path.join(__dirname, "../frontend/dist")));
-      app.get("/{*any}", (req, res) =>
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-      );
-    });
-  });
-}
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () =>
