d7c760062dcb825a7800eefa1aa81dc0f9d76cfc
Chee <tehcheeyeo@gmail.com>
2025-08-22 14:19:52 +0800

    debug: switch to axios to capture credentials

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
 
 interface User {
@@ -15,25 +16,31 @@ const AuthContext = createContext<{
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
-
   const backendUrl =
     import.meta.env.MODE === "production"
       ? "https://fruit-pos-bfoa.onrender.com"
       : "http://localhost:3000";
-
-  useEffect(() => {
-    fetch(`${backendUrl}/auth/user`, {
-      credentials: "include",
-    })
-      .then((res) => {
-        console.log(res);
-        return res.ok ? res.json() : null;
-      })
-      .then((data) => setUser(data))
-      .catch((error) => {
-        console.log("error: ", error);
+  const api = axios.create({
+    baseURL: `${backendUrl}`,
+    withCredentials: true,
+  });
+  async function getCurrentUser() {
+    try {
+      const response = await api.get("/auth/user");
+      console.log("User:", response.data);
+      setUser(response.data);
+      return response.data;
+    } catch (err: any) {
+      if (err.response?.status === 401) {
+        console.log("Not authenticated");
         setUser(null);
-      });
+        return null;
+      }
+      throw err;
+    }
+  }
+  useEffect(() => {
+    getCurrentUser();
   }, []);
 
   return (
