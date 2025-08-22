6d06d58eb894490b08d931256632fac3e5996f8a
Chee <tehcheeyeo@gmail.com>
2025-08-22 15:37:23 +0800

    debug: set secure to true

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,7 +33,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
       maxAge: 1000 * 60 * 60,
     },
