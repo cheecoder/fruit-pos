49cae041f88b9cbc355178c0c99a73805c07b9af
Chee <tehcheeyeo@gmail.com>
2025-08-23 18:54:43 +0800

    feat: provided a logout button to clear jwt token from localstorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
-import { Outlet, Link as RouterLink } from "react-router-dom";
-import { AppBar, Toolbar, IconButton, Box, Typography } from "@mui/material";
+import { Outlet, Link as RouterLink, useNavigate } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Box,
+  Typography,
+  Stack,
+} from "@mui/material";
 import IconWithBadge from "./components/IconWithBadge";
 import { useCart } from "./context/CartContext";
 import HomeIcon from "@mui/icons-material/Home";
@@ -8,11 +15,11 @@ import LoginButton from "./components/LoginButton";
 import { useAuth } from "./context/AuthContext";
 import { useEffect } from "react";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-
+import LogoutIcon from "@mui/icons-material/Logout";
 export default function App() {
   const { cart, addItem, removeItem, totalItems, totalPrice } = useCart();
   const { user, setToken, setUser } = useAuth();
-
+  const navigate = useNavigate();
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
@@ -24,12 +31,24 @@ export default function App() {
     }
   }, []);
 
+  const handleLogout = () => {
+    setToken(null);
+    setUser(null);
+    navigate("/shop");
+  };
+
   return (
     <>
       <AppBar position="static" sx={{ width: "100dvw" }}>
         <Toolbar sx={{ justifyContent: "space-between", px: "1rem" }}>
-          <Typography>{user ? `Welcome ${user.name}` : "Welcome"}</Typography>
-
+          <Stack direction={"row"} alignItems={"center"}>
+            <Typography>{user ? `Welcome ${user.name}` : "Welcome"}</Typography>
+            {user && (
+              <IconButton color="inherit" onClick={handleLogout}>
+                <LogoutIcon />
+              </IconButton>
+            )}
+          </Stack>
           {!user && <LoginButton />}
           <Box>
             <IconButton color="inherit" component={RouterLink} to="/">
