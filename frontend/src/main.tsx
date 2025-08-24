011ba1dea23684d2b4fc3b8fb69a3896398a9451
Chee <tehcheeyeo@gmail.com>
2025-08-24 22:43:23 +0800

    fix: import error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Success } from "./pages/Success";
+import Success from "./pages/Success";
 import Manage from "./pages/Manage";
 
 const queryClient = new QueryClient();
